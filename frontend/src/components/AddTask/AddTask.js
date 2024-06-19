16b38e445750d5e938f67883a1d9b6c8fde53a35
Jayaram <akulajayaram96@gmail.com>
2024-06-19 09:23:13 +0530

    Update AddTask.js

--- frontend/src/components/AddTask/AddTask.js
+++ frontend/src/components/AddTask/AddTask.js
@@ -1,4 +1,3 @@
-// AddTask.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import './AddTask.css';
@@ -13,7 +12,7 @@ const AddTask = ({ fetchTasks }) => {
   const handleAddTask = async () => {
     if (newTaskDescription.trim() !== '') {
       try {
-        await axios.post('http://localhost:5000/tasks', {
+        await axios.post('https://shanture-todo-application.onrender.com/tasks', {
           description: newTaskDescription,
           completed: false,
         });
@@ -21,6 +20,19 @@ const AddTask = ({ fetchTasks }) => {
         setNewTaskDescription(''); // Clear input field
       } catch (error) {
         console.error('Error adding task:', error);
+        if (error.response) {
+          // Server responded with a status other than 2xx
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+          console.error('Response headers:', error.response.headers);
+        } else if (error.request) {
+          // Request was made but no response received
+          console.error('Request data:', error.request);
+        } else {
+          // Something happened in setting up the request
+          console.error('Error message:', error.message);
+        }
+        alert('Error adding task. Please try again later.');
       }
     } else {
       alert('Please enter a task description');
